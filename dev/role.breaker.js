26426df21c10b959d3aed2e9758ea8a08d60caf8
BBDNET\bbdnet1321 <manus@bbd.co.za>
2022-10-10 17:49:35 +0200

    fixed breaker logic to goto target

--- dev/role.breaker.js
+++ dev/role.breaker.js
@@ -14,21 +14,19 @@ var roleBreaker = {
                         result = creep.travelTo(new RoomPosition(25,25,targetRoom),{swampCost:1,range:1});
                     return;
                 }
-                //else we're in the target room
-                else {
-                    //if no target was set, get a hostile structure to attack
-                    if(!creep.memory.target) {
-                        target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
-                        if(target && target.structureType != STRUCTURE_CONTROLLER)
-                            creep.room.target = target.id;
-                    }
+            }
+            
+            //if no target was set, get a hostile structure to attack
+            if(!creep.memory.target) {
+                target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+                if(target && target.structureType != STRUCTURE_CONTROLLER)
+                    creep.room.target = target.id;
+            }
 
-                    //continue to dismantle target
-                    var result = creep.dismantle(Game.getObjectById(creep.memory.target));
-                    if(result == ERR_NOT_IN_RANGE) {
-                        creep.travelTo(Game.getObjectById(creep.memory.target), {visualizePathStyle: {stroke: '#ffffff'}});
-                    }
-                }
+            //continue to dismantle target
+            var result = creep.dismantle(Game.getObjectById(creep.memory.target));
+            if(result == ERR_NOT_IN_RANGE) {
+                creep.travelTo(Game.getObjectById(creep.memory.target), {visualizePathStyle: {stroke: '#ffffff'}});
             }
         }
         catch (problem) {
